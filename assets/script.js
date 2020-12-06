723119c653216c15b0ea04ed1e1cdfe5e2e682e9
Puentebravo <puentematos@gmail.com>
2020-12-06 14:45:39 -0500

    Added time display to top of page

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 console.log("It's working!");
+
+$(document).ready(function () {
+  //Displays current date/time at top of page.
+  $("#currentDay").text(
+    luxon.DateTime.local().toLocaleString({
+      weekday: "long",
+      month: "long",
+      day: "2-digit",
+    })
+  );
+});
